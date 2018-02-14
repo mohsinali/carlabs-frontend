f893ef920892b1e2991a5d7feadb55099c35eaf2
Mohsin <mohammadmohsin@gmail.com>
2018-02-14 15:50:55 +0500

    sending message as object.

--- src/components/ChatBot.js
+++ src/components/ChatBot.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import _ from 'lodash';
 
 import ChatMessages from './ChatMessages';
-import { connect } from 'react-redux';
 import { addMessage } from '../actions/Messages_Action';
 import axios from 'axios';
+
 
 class ChatBot extends React.Component {
   
--- src/components/ChatMessages.js
+++ src/components/ChatMessages.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import { addMessage } from '../actions/Messages_Action';
 import MessageItem_Left from './MessageItem_Left';
 import MessageItem_Right from './MessageItem_Right';
 
@@ -10,13 +11,20 @@ class ChatMessages extends React.Component {
   }
 
   onUserMessageChange = (e) => {
-    text = e.target.value;
+    let text = e.target.value;
     this.setState({user_message: text});
   }
 
   onSubmit = (e) => {
     e.preventDefault();
-    
+    const timeStamp = Math.floor(Date.now());
+    const objMessage = {
+      _id: timeStamp,
+      message: this.state.user_message,
+      sender: 'user'
+    }
+    this.props.dispatch(addMessage(objMessage));
+    this.setState({user_message: ""});
   }
   
   render(props) {    
@@ -40,7 +48,7 @@ class ChatMessages extends React.Component {
         <div className="chat-form">
           <form role="form" onSubmit={this.onSubmit}>
             <div className="form-group">
-              <input name='txtUserMessage' className="form-control" placeholder="Message" value="" onChange={this.onUserMessageChange} />
+              <input name='txtUserMessage' className="form-control" placeholder="Message" value={this.state.user_message} onChange={this.onUserMessageChange} />
             </div>
             <div className="text-right">
               <button type="submit" className="btn btn-sm btn-primary m-t-n-xs"><strong>Send message</strong></button>
